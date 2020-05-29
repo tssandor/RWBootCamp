ff4c27efa5429c6d0e172f72e7fbf949dba21b9e
tssandor <Hello@sandor.im>
2020-05-29 18:47:05 +0800

    Preparing for HSB: hooked up labels, created max variables

--- Week01/RGBSlider/RGBSlider/ViewController.swift
+++ Week01/RGBSlider/RGBSlider/ViewController.swift
@@ -4,18 +4,28 @@
 //
 //  Created by TSS on 2020/5/29.
 //  Copyright © 2020 TSS. All rights reserved.
-// 
+//
 
 import UIKit
 
 class ViewController: UIViewController {
   
+  var usingLightColorLabels: Bool = false
+  var slider1Max: Int = 255
+  var slider2Max: Int = 255
+  var slider3Max: Int = 255
+  
+  var areWeInHSBMode: Bool = false
+  
   @IBOutlet weak var slider1: UISlider!
   @IBOutlet weak var slider2: UISlider!
   @IBOutlet weak var slider3: UISlider!
-  @IBOutlet weak var label1: UILabel!
-  @IBOutlet weak var label2: UILabel!
-  @IBOutlet weak var label3: UILabel!
+  @IBOutlet weak var sliderValueLabel1: UILabel!
+  @IBOutlet weak var sliderValueLabel2: UILabel!
+  @IBOutlet weak var sliderValueLabel3: UILabel!
+  @IBOutlet weak var sliderNameLabel1: UILabel!
+  @IBOutlet weak var sliderNameLabel2: UILabel!
+  @IBOutlet weak var sliderNameLabel3: UILabel!
   @IBOutlet weak var colorNameLabel: UILabel!
   
   override func viewDidLoad() {
@@ -29,11 +39,11 @@ class ViewController: UIViewController {
       switch sender.tag {
       // tags 1001, 1002, 1003 are slider1, slider2, slider3
       case 1001:
-        label1.text = String(Int(sliderMoved.value.rounded()))
+        sliderValueLabel1.text = String(Int(sliderMoved.value.rounded()))
       case 1002:
-        label2.text = String(Int(sliderMoved.value.rounded()))
+        sliderValueLabel2.text = String(Int(sliderMoved.value.rounded()))
       case 1003:
-        label3.text = String(Int(sliderMoved.value.rounded()))
+        sliderValueLabel3.text = String(Int(sliderMoved.value.rounded()))
       default:
         print("This should never appear, something went wrong ;]")
       }
@@ -64,15 +74,22 @@ class ViewController: UIViewController {
     sliderValueChanged(slider3)
     colorNameLabel.text = "Default white background"
     view.backgroundColor = UIColor(white: 1.0, alpha: 1.0)
+    areWeInHSBMode = false
+    toggleRGBvsHSB()
   }
   
   func changeBgColor(colorName: String) {
-    let color1 = CGFloat(slider1.value.rounded())/255
-    let color2 = CGFloat(slider2.value.rounded())/255
-    let color3 = CGFloat(slider3.value.rounded())/255
+    let color1 = CGFloat(slider1.value.rounded())/CGFloat(slider1Max)
+    let color2 = CGFloat(slider2.value.rounded())/CGFloat(slider2Max)
+    let color3 = CGFloat(slider3.value.rounded())/CGFloat(slider3Max)
     let newBgColor = CGColor(srgbRed: color1, green: color2, blue: color3, alpha: 1.0)
     colorNameLabel.text = colorName
     view.backgroundColor = UIColor(cgColor: newBgColor)
   }
+  
+  func toggleRGBvsHSB() {
+    
+  }
+  
 }
 
