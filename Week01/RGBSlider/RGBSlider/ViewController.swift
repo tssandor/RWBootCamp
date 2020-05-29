110648da6f3c01425781451bc05cd09738a665d8
tssandor <Hello@sandor.im>
2020-05-29 16:49:28 +0800

    Main UI elements in place

--- Week01/RGBSlider/RGBSlider/ViewController.swift
+++ Week01/RGBSlider/RGBSlider/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet weak var slider1: UISlider!
+  @IBOutlet weak var slider2: UISlider!
+  @IBOutlet weak var slider3: UISlider!
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
+  @IBAction func setColor() {
+    print("R: \(slider1.value), G: \(slider2.value), B: \(slider3.value)")
+  }
 
+  @IBAction func reset() {
+    slider1.value = 0.0
+    slider2.value = 0.0
+    slider3.value = 0.0
+  }
+  
 }
 
