da3c2eda2848e60e16a9750e07ee4bc5602805ad
tssandor <Hello@sandor.im>
2020-06-22 14:14:19 +0800

    Homework finished

--- Week04/CompatibilitySlider-Start/CompatibilitySlider-Start/ViewController.swift
+++ Week04/CompatibilitySlider-Start/CompatibilitySlider-Start/ViewController.swift
@@ -14,8 +14,8 @@ class ViewController: UIViewController {
   @IBOutlet weak var slider: UISlider!
   @IBOutlet weak var questionLabel: UILabel!
 
-  var compatibilityItems = ["Cats", "Dogs", "Long string for debugging", "Gym", "Cold weather", "Reading"] // Add more!
-  var currentItemIndex: Int = 0
+  var compatibilityItems = ["Cats", "Dogs", "Debugging for hours", "Gym", "Cold weather", "Reading"]
+  var currentItemIndex: Int = -1
 
   var person1 = Person(id: 1, items: [:])
   var person2 = Person(id: 2, items: [:])
@@ -25,12 +25,11 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     startGame()
   }
-
-  @IBAction func sliderValueChanged(_ sender: UISlider) {
-    // print(sender.value)
-//      let currentItem = compatibilityItems[currentItemIndex]
-//      currentPerson?.items.updateValue(slider.value, forKey: currentItem)
-
+  
+  func startGame() {
+    questionLabel.text = "User 1, how do you feel about..."
+    currentPerson = person1
+    askAQuestion()
   }
 
   @IBAction func didPressNextItemButton(_ sender: Any) {
@@ -38,6 +37,47 @@ class ViewController: UIViewController {
     askAQuestion()
   }
 
+  func saveValue() {
+    let currentItem = compatibilityItems[currentItemIndex]
+    currentPerson!.items.updateValue(slider.value, forKey: currentItem)
+    slider.value = 2.5
+  }
+
+  func checkIfStillQuestionsLeft() -> Bool {
+    currentItemIndex < compatibilityItems.count ? true : false
+  }
+    
+  func askAQuestion() {
+    currentItemIndex += 1
+    // Let's check if we have any questions left!
+    if !checkIfStillQuestionsLeft() {
+      if currentPerson == person2 {
+        // No more questions and we are already at person2, so let's evaluate
+        evaluateCompatibility()
+      } else {
+        // No more questions for person1, let's switch to person2!
+        currentPerson = person2
+        currentItemIndex = -1
+        questionLabel.text = "User 2, how do you feel about..."
+        askAQuestion()
+      }
+    } else {
+      // There are still questions left!
+      compatibilityItemLabel.text = compatibilityItems[currentItemIndex]
+    }
+  }
+  
+  func evaluateCompatibility() {
+    let message = "You two are \(calculateCompatibility()) compatible"
+    let alert = UIAlertController(title: "The results are in!", message: message, preferredStyle: .alert)
+    let action = UIAlertAction(title: "Restart game", style: .default, handler: {
+      action in
+      self.startGame()
+    })
+    alert.addAction(action)
+    present(alert, animated: true, completion: nil)
+  }
+    
   func calculateCompatibility() -> String {
     // If diff 0.0 is 100% and 5.0 is 0%, calculate match percentage
     var percentagesForAllItems: [Double] = []
@@ -53,60 +93,6 @@ class ViewController: UIViewController {
     print(matchPercentage, "%")
     let matchString = 100 - (matchPercentage * 100).rounded()
     return "\(matchString)%"
-  }
-  
-  func checkIfStillQuestionsLeft() -> Bool {
-    currentItemIndex < compatibilityItems.count ? true : false
-  }
-  
-  func theNextItem() -> String? {
-//    currentItemIndex += 1
-    if currentItemIndex < compatibilityItems.count {
-      return compatibilityItems[currentItemIndex]
-    } else {
-      return nil
-    }
-  }
-  
-  func evaluateCompatibility() {
-    let message = "You two are \(calculateCompatibility()) compatible"
-    let alert = UIAlertController(title: "The results are in!", message: message, preferredStyle: .alert)
-    let action = UIAlertAction(title: "Restart game", style: .default, handler: {
-      action in
-      self.startGame()
-    })
-    alert.addAction(action)
-    present(alert, animated: true, completion: nil)
-  }
-  
-  func saveValue() {
-    let currentItem = compatibilityItems[currentItemIndex]
-    currentPerson!.items.updateValue(slider.value, forKey: currentItem)
-    slider.value = 3.0
-  }
-  
-  func askAQuestion() {
-    currentItemIndex += 1
-    if currentPerson == person2 && !checkIfStillQuestionsLeft() {
-       evaluateCompatibility()
-     } else if !checkIfStillQuestionsLeft() {
-       currentPerson = person2
-       currentItemIndex = -1
-       print("Switch to person2")
-       questionLabel.text = "User 2, how do you feel about..."
-       askAQuestion()
-    } else {
-       compatibilityItemLabel.text = compatibilityItems[currentItemIndex]
-    }
-//     }
-     
-  }
-  
-  func startGame() {
-    questionLabel.text = "User 1, how do you feel about..."
-    currentPerson = person1
-    currentItemIndex = -1
-    askAQuestion()
   }
 
 }
