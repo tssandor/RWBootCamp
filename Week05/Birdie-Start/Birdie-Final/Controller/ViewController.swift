63de314cfed82d7549090fac2b3b5ee62ec65bb0
tssandor <Hello@sandor.im>
2020-06-28 12:04:13 +0800

    TextPostCell UI done

--- Week05/Birdie-Start/Birdie-Final/Controller/ViewController.swift
+++ Week05/Birdie-Start/Birdie-Final/Controller/ViewController.swift
@@ -8,27 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BirdieViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    @IBOutlet weak var tableview: UITableView!
+  @IBOutlet weak var tableview: UITableView!
+//  @IBOutlet weak var avatar: UIImage!
+//  @IBOutlet weak var username: UILabel!
+//  @IBOutlet weak var postDate: UILabel!
+//  @IBOutlet weak var postText: UILabel!
+  
+  override func viewDidLoad() {
+      super.viewDidLoad()
+      setUpTableView()
+  }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setUpTableView()
-    }
+  func setUpTableView() {
+    tableview.delegate = self
+    tableview.dataSource = self
+    MediaPostsHandler.shared.getPosts()
+    // Set delegates, register custom cells, set up datasource, etc.
+  }
 
-    func setUpTableView() {
-        // Set delegates, register custom cells, set up datasource, etc.
-    }
+  @IBAction func didPressCreateTextPostButton(_ sender: Any) {
 
-    @IBAction func didPressCreateTextPostButton(_ sender: Any) {
+  }
 
-    }
+  @IBAction func didPressCreateImagePostButton(_ sender: Any) {
 
-    @IBAction func didPressCreateImagePostButton(_ sender: Any) {
+  }
 
-    }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    let x = MediaPostsHandler.shared.mediaPosts.count
+    return x
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "TextPostCell", for: indexPath)
+    
+//    if let avatar = cell.viewWithTag(1001) as? UIImage {
+//      avatar.
+//    }
+    
+    return cell
+  }
 
+  
 }
 
 
