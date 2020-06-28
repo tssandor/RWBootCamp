33331ef47f4e479d211508be8e1364e6e9a015b4
tssandor <Hello@sandor.im>
2020-06-28 13:21:16 +0800

    All display features work (no add post)

--- Week05/Birdie-Start/Birdie-Final/Controller/ViewController.swift
+++ Week05/Birdie-Start/Birdie-Final/Controller/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class BirdieViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
   @IBOutlet weak var tableview: UITableView!
-//  @IBOutlet weak var avatar: UIImage!
+  
 //  @IBOutlet weak var username: UILabel!
 //  @IBOutlet weak var postDate: UILabel!
 //  @IBOutlet weak var postText: UILabel!
@@ -42,13 +42,48 @@ class BirdieViewController: UIViewController, UITableViewDelegate, UITableViewDa
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "TextPostCell", for: indexPath)
     
-//    if let avatar = cell.viewWithTag(1001) as? UIImage {
-//      avatar.
-//    }
+    var cell = tableView.dequeueReusableCell(withIdentifier: "TextPostCell", for: indexPath)
+    var itsATextPost = true
+    
+    // I decided to make this a function to make sure currentPost is available outside the if scope.
+    // This results in cleaner code, as we don't need set up labels inside the if statements.
+    let currentPost = queryDataForCurrentCell(atIndex: indexPath.row)
+
+    if currentPost is TextPost {
+      cell = tableView.dequeueReusableCell(withIdentifier: "TextPostCell", for: indexPath)
+      itsATextPost = true
+    } else if currentPost is ImagePost {
+      cell = tableView.dequeueReusableCell(withIdentifier: "ImagePostCell", for: indexPath)
+      itsATextPost = false
+    }
+    
+    print(currentPost)
+
+    if let username = cell.viewWithTag(1001) as? UILabel {
+      username.text = currentPost.userName
+    }
+    if let posttext = cell.viewWithTag(1003) as? UILabel {
+      posttext.text = currentPost.textBody!
+    }
+    if !itsATextPost {
+      let currentImagePost = currentPost as? ImagePost
+      if let postImage = cell.viewWithTag(2001) as? UIImageView {
+        postImage.image = currentImagePost?.image
+      }
+    }
     
     return cell
+  }
+  
+  func queryDataForCurrentCell(atIndex: Int) -> MediaPost {
+    if let post = MediaPostsHandler.shared.mediaPosts[atIndex] as? TextPost {
+      return post
+    } else if let post = MediaPostsHandler.shared.mediaPosts[atIndex] as? ImagePost {
+      return post
+    }
+    // This should never execute
+    return MediaPostsHandler.shared.mediaPosts[0]
   }
 
   
